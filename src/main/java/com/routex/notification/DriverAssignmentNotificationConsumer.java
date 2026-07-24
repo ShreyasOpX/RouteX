53286526259f29e94e85301b65f90f6a86220603
= <shreyas.ricky41@gmail.com>
2026-07-24 19:53:47 +0530

    consume driver assignment events

---
+++ src/main/java/com/routex/notification/DriverAssignmentNotificationConsumer.java
@@ -0,0 +1,20 @@
+package com.routex.notification;
+
+import com.routex.dispatch.KafkaTopicConfiguration;
+import com.routex.matching.DriverAssignmentEvent;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DriverAssignmentNotificationConsumer {
+    @KafkaListener(topics = KafkaTopicConfiguration.DRIVER_ASSIGNED_TOPIC)
+    public void handleDriverAssigned(DriverAssignmentEvent event) {
+        System.out.printf(
+                "NOTIFICATION: Passenger %s - Driver %s (%s) has been assigned to ride %s%n",
+                event.passengerId(),
+                event.driverName(),
+                event.vehicleNumber(),
+                event.rideId()
+        );
+    }
+}
