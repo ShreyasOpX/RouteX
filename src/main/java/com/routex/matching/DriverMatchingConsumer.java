4ab51ed63fb5061ad742d2cc013fdbdbe6ae2a76
= <shreyas.ricky41@gmail.com>
2026-07-26 23:06:06 +0530

    added offset observability

--- src/main/java/com/routex/matching/DriverMatchingConsumer.java
+++ src/main/java/com/routex/matching/DriverMatchingConsumer.java
@@ -19,7 +19,13 @@ public class DriverMatchingConsumer {
     }
 
     @KafkaListener(topics = KafkaTopicConfiguration.RIDE_REQUESTED_TOPIC, groupId = "driver-matching-group")
-    public void handleRideRequested(RideRequestedEvent event, @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) {
+    public void handleRideRequested(RideRequestedEvent event, @Header(KafkaHeaders.RECEIVED_PARTITION) int partition, @Header(KafkaHeaders.OFFSET) long offset) {
+        System.out.printf(
+                "MATCHING | ride=%s | partition=%d | offset=%d%n",
+                event.rideId(),
+                partition,
+                offset
+        );
         DriverAssignmentEvent assignment = driverAssignmentService.assignDriver(event);
 //        System.out.printf(
 //                "MATCHING: Driver %s (%s) assigned to ride %s%n",
@@ -28,10 +34,6 @@ public class DriverMatchingConsumer {
 //                assignment.rideId()
 //        );
         driverAssignmentProducer.publish(assignment);
-        System.out.printf(
-                "MATCHING | ride=%s | partition=%d%n",
-                event.rideId(),
-                partition
-        );
+
     }
 }
